44f4de0a772936633ec4613ac5ef5ee89d2d3268
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2025-10-10 14:08:21 +0900

    fix: wsl script

--- scripts/pwsh/enable-wsl-no-store.ps1
+++ scripts/pwsh/enable-wsl-no-store.ps1
@@ -5,9 +5,9 @@ if (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Adminis
     return
 }
 $KERNEL_DOWNLOAD_URL='https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi'
-$UBUNTU2004_DOWNLOAD_URL='https://wslstorestorage.blob.core.windows.net/wslblob/Ubuntu2404-240425.AppxBundle'
+$UBUNTU2004_DOWNLOAD_URL='https://releases.ubuntu.com/24.04.3/ubuntu-24.04.3-wsl-amd64.wsl'
 $MSI_FILE='temp.msi'
-$APPX_FILE='temp.appx'
+$UBUNTU_FILE='temp.appx'
 # see https://learn.microsoft.com/en-us/windows/wsl/install-manual
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
@@ -15,12 +15,12 @@ Invoke-WebRequest -Uri $KERNEL_DOWNLOAD_URL -OutFile $MSI_FILE
 Start-Process -FilePath $MSI_FILE -ArgumentList "/quiet"
 wsl --set-default-version 2
 wsl --update --web-download
-Invoke-WebRequest -Uri $UBUNTU2004_DOWNLOAD_URL -OutFile $APPX_FILE
+Invoke-WebRequest -Uri $UBUNTU2004_DOWNLOAD_URL -OutFile $UBUNTU_FILE
 # if Add-AppPackage failed, try import Appx module first
-# Import-Module Appx 
+# Import-Module Appx
 # if still failing, try using the usewindowspowershell option
 # Import-Module Appx -usewindowspowershell
-Add-AppPackage $APPX_FILE
+wsl --install --from-file $UBUNTU_FILE
 Remove-Item -Path $MSI_FILE
 Remove-Item -Path $APPX_FILE
 winget install -e --id Microsoft.WindowsTerminal
