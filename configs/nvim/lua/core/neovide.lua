f7db200bd4e14630476d5f244fadf4908c9092c4
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2025-12-15 16:39:55 +0900

    update nvim config

--- configs/nvim/lua/core/neovide.lua
+++ configs/nvim/lua/core/neovide.lua
@@ -1,13 +1,13 @@
 vim.o.guifont = "CaskaydiaMono NFM:i:h11"
 vim.keymap.set({ "n", "v" }, "<C-v>", [["+p]])
-vim.keymap.set("i", "<C-v>", "<C-r>+")
+vim.keymap.set({ "i", "c" }, "<C-v>", "<C-r>+")
 local mainColor = string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
 vim.g.neovide_title_background_color = mainColor
 vim.g.neovide_title_text_color = mainColor
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 vim.g.neovide_input_ime = true
-local animation = false
+local animation = true
 if animation then
     vim.g.neovide_cursor_animate_in_insert_mode = true
     vim.g.neovide_cursor_animate_command_line = true
