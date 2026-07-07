a59b6d64abc495e142eee76c9bb02ab9dc73cbff
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2026-07-07 17:10:46 +0900

    upd

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -2,9 +2,9 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 require("config.autocmd")
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("gopls")
-vim.lsp.enable("vtsls")
+for _, path in ipairs(vim.fn.glob(vim.fn.stdpath("config") .. "/lsp/*.lua", false, true)) do
+    vim.lsp.enable(vim.fn.fnamemodify(path, ":t:r"))
+end
 vim.cmd("colorscheme doom-one")
 if vim.g.neovide then
     require("config.neovide")
