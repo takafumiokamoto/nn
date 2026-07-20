447ecbf36325070c1ddd183067fcb63c17b06d1a
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2026-07-20 22:40:28 +0900

    upd

--- install/unix.sh
+++ install/unix.sh
@@ -5,10 +5,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 MISE_CONFIG="$PROJECT_ROOT/configs/mise/config.toml"
 
-if ! command -v curl > /dev/null 2>&1; then
+if ! command -v curl >/dev/null 2>&1; then
     echo "curl was not found. Please install it before running the script."
+    exit 1
 fi
-if command -v mise > /dev/null 2>&1; then
+if command -v mise >/dev/null 2>&1; then
     MISE_BIN="$(command -v mise)"
 elif [[ -x "${HOME}/.local/bin/mise" ]]; then
     # miseがPATHにないが、インストールされている場合
@@ -22,10 +23,10 @@ export MISE_GLOBAL_CONFIG_FILE="${MISE_CONFIG}"
 if [[ -f "${PROJECT_ROOT}/.env" ]]; then
     export MISE_ENV_FILE="${PROJECT_ROOT}/.env"
     tmp=$(mktemp)
-    "${MISE_BIN}" env -s zsh | awk '/^export DOTFILE_ENV_/ { print }' > "$tmp"
+    "${MISE_BIN}" env -s zsh | awk '/^export DOTFILE_ENV_/ { print }' >"$tmp"
     mv "$tmp" "$HOME/.env.dotfile"
 else
-    rm -f $HOME/.env.dotfile
+    rm -f "$HOME/.env.dotfile"
 fi
 
 "${MISE_BIN}" bootstrap --yes --force-dotfiles
